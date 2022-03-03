5c10e1f51a70eb687ecaebcd5d11ce8ae21a20fd
Aymeric <aymeric.legros@adixon.fr>
2022-03-03 16:44:05 +0100

    first commit

---
+++ app/src/main/java/fr/adixon/adiposrfiddemo/MainActivity.kt
@@ -0,0 +1,115 @@
+package fr.adixon.adiposrfiddemo
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.*
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var mContext: Context? = null
+    private var mIsBound = false
+    private var randomNumberValue = 0
+    var randomNumberRequestMessenger: Messenger? = null
+    var randomNumberReceiveMessenger: Messenger? = null
+    private var serviceIntent: Intent? = null
+    private var textViewRandomNumber: TextView? = null
+    private var buttonBindService: Button? = null
+    private var buttonUnBindService: Button? = null
+    private var buttonGetRandomNumber: Button? = null
+
+    internal inner class RecieveRandomNumberHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            randomNumberValue = 0
+            when (msg.what) {
+                GET_RANDOM_NUMBER_FLAG -> {
+                    randomNumberValue = msg.arg1
+                    textViewRandomNumber?.text = "Random Number: $randomNumberValue"
+                }
+                else -> {}
+            }
+            super.handleMessage(msg)
+        }
+    }
+
+    private var randomNumberServiceConnection: ServiceConnection? = object : ServiceConnection {
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            randomNumberRequestMessenger = null
+            randomNumberReceiveMessenger = null
+            mIsBound = false
+        }
+
+        override fun onServiceConnected(arg0: ComponentName, binder: IBinder) {
+            randomNumberRequestMessenger = Messenger(binder)
+            randomNumberReceiveMessenger = Messenger(RecieveRandomNumberHandler())
+            mIsBound = true
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mContext = applicationContext
+        textViewRandomNumber = findViewById<TextView>(R.id.textViewRandomNumber)
+        buttonBindService = findViewById<Button>(R.id.buttonBindService)
+        buttonUnBindService = findViewById<Button>(R.id.buttonUnBindService)
+        buttonGetRandomNumber = findViewById<Button>(R.id.buttonGetRandomNumber)
+        buttonGetRandomNumber?.setOnClickListener(this)
+        buttonBindService?.setOnClickListener(this)
+        buttonUnBindService?.setOnClickListener(this)
+        serviceIntent = Intent()
+        serviceIntent!!.component = ComponentName("fr.adixon.adiposrfid", "fr.adixon.adiposrfid.MyService")
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.buttonBindService -> bindToRemoteService()
+            R.id.buttonUnBindService -> unbindFromRemoteSevice()
+            R.id.buttonGetRandomNumber -> fetchRandomNumber()
+            else -> {}
+        }
+    }
+
+    private fun bindToRemoteService() {
+        randomNumberServiceConnection?.let { bindService(serviceIntent, it, BIND_AUTO_CREATE) }
+        Toast.makeText(mContext, "Service bound", Toast.LENGTH_SHORT).show()
+        mIsBound = true;
+    }
+
+    private fun unbindFromRemoteSevice() {
+        if (mIsBound) {
+            randomNumberServiceConnection?.let { unbindService(it) }
+            mIsBound = false
+            Toast.makeText(mContext, "Service Unbound", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun fetchRandomNumber() {
+        if (mIsBound) {
+            val requestMessage = Message.obtain(null, GET_RANDOM_NUMBER_FLAG)
+            requestMessage.replyTo = randomNumberReceiveMessenger
+            try {
+                randomNumberRequestMessenger?.send(requestMessage)
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+        } else {
+            Toast.makeText(mContext, "Service Unbound, can't get random number", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        randomNumberServiceConnection = null
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        const val GET_RANDOM_NUMBER_FLAG = 0
+    }
+}
